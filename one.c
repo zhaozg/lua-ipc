2348daf39865cef84d5c0908488dc1444c743c7e
zhaozg <zhaozg@gmail.com>
2017-04-01 13:49:22 +0800

    add one.c file include all code

---
+++ one.c
@@ -0,0 +1,10 @@
+#ifdef LUA_IPC_ONE
+#include "flock.c"
+#include "ipc.c"
+#include "memfile.c"
+#include "mmap.c"
+#include "proc.c"
+#include "sem.c"
+#include "shm.c"
+#include "strfile.c"
+#endif
